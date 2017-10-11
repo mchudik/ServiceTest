48d636e51930e98103f7899485653c76f8d59af3
Martin Chudik <mchudik@echo360.com>
2017-10-11 17:43:29 -0400

    Modifying security descriptor on mapped object

--- Service/CppWindowsService/SampleService.cpp
+++ Service/CppWindowsService/SampleService.cpp
@@ -25,79 +25,24 @@
 #include <stdio.h>
 #include <conio.h>
 #include <tchar.h>
+#include <aclapi.h>
 
 #define WIDTH 1920
 #define HEIGHT 1080
 #define BUF_SIZE WIDTH * HEIGHT * 2
 #define FPS 1000/33
 
-LPCTSTR lpszPrivilege = TEXT("SeCreateGlobalPrivilege");
 TCHAR szName[] = TEXT("Global\\MyFileMappingObject");
 HANDLE hMapFile = nullptr;
 HANDLE hToken = nullptr;
 LPCTSTR pBuf = nullptr;
 BYTE pixel = 0;
 
-BOOL SetPrivilege(
-	HANDLE hToken,           // access token handle
-	LPCTSTR lpszPrivilege,   // name of privilege to enable/disable
-	BOOL bEnablePrivilege    // to enable (or disable privilege)
-)
-{
-	// Token privilege structure
-	TOKEN_PRIVILEGES tp;
-	// Used by local system to identify the privilege
-	LUID luid;
-
-	if (!LookupPrivilegeValue(
-		NULL,                // lookup privilege on local system
-		lpszPrivilege,    // privilege to lookup
-		&luid))               // receives LUID of privilege
-	{
-		printf("LookupPrivilegeValue() error: %u\n", GetLastError());
-		return FALSE;
-	} else
-		printf("LookupPrivilegeValue() is OK\n");
-
-	tp.PrivilegeCount = 1;
-	tp.Privileges[0].Luid = luid;
-
-	// Don't forget to disable the privileges after you enabled them,
-	// or have already completed your task. Don't mess up your system :o)
-	if (bEnablePrivilege)
-	{
-		tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-		printf("tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED\n");
-	} else
-	{
-		tp.Privileges[0].Attributes = 0;
-		printf("tp.Privileges[0].Attributes = 0\n");
-	}
-
-	// Enable the privilege (or disable all privileges).
-	if (!AdjustTokenPrivileges(
-		hToken,
-		FALSE, // If TRUE, function disables all privileges, if FALSE the function modifies privilege based on the tp
-		&tp,
-		sizeof(TOKEN_PRIVILEGES),
-		(PTOKEN_PRIVILEGES)NULL,
-		(PDWORD)NULL))
-	{
-		printf("AdjustTokenPrivileges() error: %u\n", GetLastError());
-		return FALSE;
-	} else
-	{
-		printf("AdjustTokenPrivileges() is OK, last error if any: %u\n", GetLastError());
-		printf("Should be 0, means the operation completed successfully = ERROR_SUCCESS\n");
-	}
-	return TRUE;
-}
-
-BOOL CreateAndMapSharedMemory(HANDLE *hMapFile, LPCTSTR *pBuf)
+BOOL CreateAndMapSharedMemory(HANDLE *hMapFile, LPCTSTR *pBuf, LPSECURITY_ATTRIBUTES lpAttributes)
 {
 	*hMapFile = CreateFileMapping(
 		INVALID_HANDLE_VALUE,    // use paging file
-		NULL,                    // default security
+		lpAttributes,            // security attributes or NULL for default security
 		PAGE_READWRITE,          // read/write access
 		0,                       // maximum object size (high-order DWORD)
 		BUF_SIZE,                // maximum object size (low-order DWORD)
@@ -134,6 +79,121 @@ VOID UnMapSharedMemory(HANDLE hMapFile, LPCTSTR pBuf)
 	CloseHandle(&hMapFile);
 }
 
+BOOL CreateAndMapSharedMemoryWithSecurity(HANDLE *hMapFile, LPCTSTR *pBuf)
+{
+
+	DWORD dwRes, dwDisposition;
+	PSID pUserSID = NULL, pAdminSID = NULL;
+	PACL pACL = NULL;
+	PSECURITY_DESCRIPTOR pSD = NULL;
+	EXPLICIT_ACCESS ea[2];
+	SID_IDENTIFIER_AUTHORITY SIDAuthUser = SECURITY_LOCAL_SID_AUTHORITY;
+	SID_IDENTIFIER_AUTHORITY SIDAuthNT = SECURITY_NT_AUTHORITY;
+	SECURITY_ATTRIBUTES sa;
+	LONG lRes;
+	HKEY hkSub = NULL;
+	BOOL retval = FALSE;
+
+	// Create a well-known SID for the Logged on User.
+	if (!AllocateAndInitializeSid(&SIDAuthUser, 1,
+		SECURITY_LOCAL_LOGON_RID,
+		0, 0, 0, 0, 0, 0, 0,
+		&pUserSID))
+	{
+		_tprintf(_T("AllocateAndInitializeSid Error %u\n"), GetLastError());
+		goto Cleanup;
+	}
+
+	// Initialize an EXPLICIT_ACCESS structure for an ACE.
+	// The ACE will allow Logged on User to access shared memory.
+	ZeroMemory(&ea, 2 * sizeof(EXPLICIT_ACCESS));
+	ea[0].grfAccessPermissions = FILE_MAP_ALL_ACCESS;
+	ea[0].grfAccessMode = SET_ACCESS;
+	ea[0].grfInheritance = NO_INHERITANCE;
+	ea[0].Trustee.TrusteeForm = TRUSTEE_IS_SID;
+	ea[0].Trustee.TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP;
+	ea[0].Trustee.ptstrName = (LPTSTR)pUserSID;
+
+	// Create a SID for the BUILTIN\Administrators group.
+	if (!AllocateAndInitializeSid(&SIDAuthNT, 2,
+		SECURITY_BUILTIN_DOMAIN_RID,
+		DOMAIN_ALIAS_RID_ADMINS,
+		0, 0, 0, 0, 0, 0,
+		&pAdminSID))
+	{
+		_tprintf(_T("AllocateAndInitializeSid Error %u\n"), GetLastError());
+		goto Cleanup;
+	}
+
+	// Initialize an EXPLICIT_ACCESS structure for an ACE.
+	// The ACE will allow the Administrators group full access to the shared memory
+	ea[1].grfAccessPermissions = FILE_MAP_ALL_ACCESS;
+	ea[1].grfAccessMode = SET_ACCESS;
+	ea[1].grfInheritance = NO_INHERITANCE;
+	ea[1].Trustee.TrusteeForm = TRUSTEE_IS_SID;
+	ea[1].Trustee.TrusteeType = TRUSTEE_IS_GROUP;
+	ea[1].Trustee.ptstrName = (LPTSTR)pAdminSID;
+
+	// Create a new ACL that contains the new ACEs.
+	dwRes = SetEntriesInAcl(2, ea, NULL, &pACL);
+	if (ERROR_SUCCESS != dwRes)
+	{
+		_tprintf(_T("SetEntriesInAcl Error %u\n"), GetLastError());
+		goto Cleanup;
+	}
+
+	// Initialize a security descriptor.  
+	pSD = (PSECURITY_DESCRIPTOR)LocalAlloc(LPTR,
+		SECURITY_DESCRIPTOR_MIN_LENGTH);
+	if (NULL == pSD)
+	{
+		_tprintf(_T("LocalAlloc Error %u\n"), GetLastError());
+		goto Cleanup;
+	}
+
+	if (!InitializeSecurityDescriptor(pSD,
+		SECURITY_DESCRIPTOR_REVISION))
+	{
+		_tprintf(_T("InitializeSecurityDescriptor Error %u\n"),
+			GetLastError());
+		goto Cleanup;
+	}
+
+	// Add the ACL to the security descriptor. 
+	if (!SetSecurityDescriptorDacl(pSD,
+		TRUE,     // bDaclPresent flag   
+		pACL,
+		FALSE))   // not a default DACL 
+	{
+		_tprintf(_T("SetSecurityDescriptorDacl Error %u\n"),
+			GetLastError());
+		goto Cleanup;
+	}
+
+	// Initialize a security attributes structure.
+	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+	sa.lpSecurityDescriptor = pSD;
+	sa.bInheritHandle = FALSE;
+
+	// Use the security attributes to set the security descriptor 
+	retval = CreateAndMapSharedMemory(hMapFile, pBuf, &sa);
+
+Cleanup:
+
+	if (pUserSID)
+		FreeSid(pUserSID);
+	if (pAdminSID)
+		FreeSid(pAdminSID);
+	if (pACL)
+		LocalFree(pACL);
+	if (pSD)
+		LocalFree(pSD);
+	if (hkSub)
+		RegCloseKey(hkSub);
+
+	return retval;
+
+}
 
 CSampleService::CSampleService(PWSTR pszServiceName, 
 							   BOOL fCanStop, 
@@ -194,27 +254,8 @@ void CSampleService::OnStart(DWORD dwArgc, LPWSTR *lpszArgv)
 	WriteEventLogEntry(L"CppWindowsService in OnStart", 
 		EVENTLOG_INFORMATION_TYPE);
 
-	// Open a handle to the access token for the calling process. That is this running program
-	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken))
-	{
-		WriteEventLogEntry(L"CppWindowsService Error in OpenProcessToken()!",
-			EVENTLOG_ERROR_TYPE);
-	} else {
-		WriteEventLogEntry(L"CppWindowsService OpenProcessToken() succeeded!",
-			EVENTLOG_INFORMATION_TYPE);
-	}
-
-	// Call the user defined SetPrivilege() function to enable and set the needed privilege
-	if (SetPrivilege(hToken, lpszPrivilege, TRUE)) {
-		WriteEventLogEntry(L"CppWindowsService SetPrivilege() succeeded!",
-			EVENTLOG_INFORMATION_TYPE);
-	} else {
-		WriteEventLogEntry(L"CppWindowsService Error in SetPrivilege()!",
-			EVENTLOG_ERROR_TYPE);
-	}
-
 	// Create and map shared memory
-	if (CreateAndMapSharedMemory(&hMapFile, &pBuf)) {
+	if (CreateAndMapSharedMemoryWithSecurity(&hMapFile, &pBuf)) {
 		WriteEventLogEntry(L"CppWindowsService Mapped Memory OK!",
 			EVENTLOG_INFORMATION_TYPE);
 	} else {
@@ -297,14 +338,5 @@ void CSampleService::OnStop()
 		UnMapSharedMemory(hMapFile, pBuf);
 		WriteEventLogEntry(L"CppWindowsService Unmapped shared memory",
 			EVENTLOG_INFORMATION_TYPE);
-	}
-
-	// After we have completed our task, don't forget to disable the privilege
-	if (SetPrivilege(hToken, lpszPrivilege, FALSE)) {
-		WriteEventLogEntry(L"CppWindowsService SetPrivilege() back succeeded!",
-			EVENTLOG_INFORMATION_TYPE);
-	} else {
-		WriteEventLogEntry(L"CppWindowsService Error in SetPrivilege() back!",
-			EVENTLOG_ERROR_TYPE);
 	}
 }
